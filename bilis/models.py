115171ca2eed2dea358d9c629da9ad45d45a746f
Matti Vesikivi <matti.vesikivi@iki.fi>
2013-12-18 23:38:01 +0200

    added preliminary models

--- bilis/models.py
+++ bilis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    score = models.FloatField()
+    favorite_color = models.IntegerField()
+
+class Game(models.Model):
+    winner = models.ForeignKey(Player, related_name="won_games")
+    loser = models.ForeignKey(Player, related_name="lost_games")
+    under_table = models.BooleanField()
+
+
+    
