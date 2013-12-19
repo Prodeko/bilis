3b041a0a0c9525a2482bfa57fc9315766621f619
Matti Vesikivi <matti.vesikivi@iki.fi>
2013-12-19 05:39:16 +0200

    added datetime to Game class + unicode representation for objects

--- bilis/models.py
+++ bilis/models.py
@@ -6,11 +6,15 @@ class Player(models.Model):
     name = models.CharField(max_length=100)
     score = models.FloatField()
     favorite_color = models.IntegerField()
+    def __unicode__(self):
+    	return self.name
 
 class Game(models.Model):
     winner = models.ForeignKey(Player, related_name="won_games")
     loser = models.ForeignKey(Player, related_name="lost_games")
+    datetime = models.DateTimeField(auto_now=True)
     under_table = models.BooleanField()
-
+    def __unicode__(self):
+        return self.winner.name + " vs. " + self.loser.name + " " + self.datetime.strftime("%Y-%m-%d")
 
     
