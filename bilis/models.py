65a6ad5a93ebe05f771a86be8e24f0f53d231c90
kristiansjolund <kristian@pulmaton.fi>
2014-02-06 20:56:05 +0200

    Ratings calculation imblemendadion

--- bilis/models.py
+++ bilis/models.py
@@ -4,11 +4,15 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
-    score = models.IntegerField()
-    live_score = models.IntegerField()
+    rating = models.IntegerField()
+    live_rating = models.IntegerField()
     favorite_color = models.IntegerField()
     def __unicode__(self):
     	return self.name
+    def calculate_rating(self, opponent_rating, result)
+        change = result * (self.live_rating / opponent_rating) * 10
+        self.live_rating = self.live_rating + change
+        return change 
 
 class Game(models.Model):
     winner = models.ForeignKey(Player, related_name="won_games")
@@ -18,5 +22,11 @@ class Game(models.Model):
     notes = models.TextField(blank=True)
     def __unicode__(self):
         return self.winner.name + " vs. " + self.loser.name + " " + self.datetime.strftime("%Y-%m-%d")
+    def calculate_ratings(self)
+        winner_rating = winner.live_rating
+        winner.calculate_rating(opponent.live_rating, 1)
+        loser.calculate_rating(winner_rating, -1)
+    post_save.connect(calculate_ratings)
+        
 
     
