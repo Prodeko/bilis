26fc441575c0caefcb9703a898d9ca91bfa7e173
Matti Vesikivi <matti.vesikivi@iki.fi>
2014-03-05 21:03:08 +0200

    added tweak to the wsgi script to find settings

--- biliskilke/wsgi.py
+++ biliskilke/wsgi.py
@@ -14,7 +14,10 @@ framework.
 
 """
 import os
+import sys
 
+root = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, root)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biliskilke.settings")
 
 # This application object is used by any WSGI server configured to use this
