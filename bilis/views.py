4f2f405cd50083079de0898c70945356b1826ab6
Matti Vesikivi <matti.vesikivi@iki.fi>
2014-03-11 20:48:16 +0200

    new index page layout

--- bilis/views.py
+++ bilis/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render_to_response, redirect, render
 from django.forms import ModelForm
 from django.template import RequestContext
-from bilis.models import Player
+from bilis.models import Player, Game
 from bilis.forms import PlayerForm, ResultForm
 
 def index(request):
     form = ResultForm()
-    players = Player.objects.all().order_by('-live_rating')
+    players = Player.objects.all().order_by('-live_rating')[:20]
+    latest_games = Game.objects.all().order_by('-datetime')[:20]
     return render_to_response('index.html',{
                 'form': form,
-                'players': players
+                'players': players,
+                'latest_games' : latest_games
         }, context_instance=RequestContext(request))
 
 def add_result(request):
