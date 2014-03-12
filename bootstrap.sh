921f4c67fc51993e45e1a6f9a18218278d692e33
Matti Vesikivi <matti.vesikivi@iki.fi>
2014-03-12 15:03:21 +0200

    Make vagrant start up the django test server when logging in. Remove unnecessary sudo's from bootstrap.sh. Dont install mod_php5

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,58 +1,52 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+apt-get update
 # Required for add-apt-repository
-sudo apt-get install -y software-properties-common python-software-properties
+apt-get install -y software-properties-common python-software-properties
 
 # Add required repositories
 # node.js
-sudo add-apt-repository -y ppa:chris-lea/node.js >> /tmp/add-apt-errors 2>&1
+add-apt-repository -y ppa:chris-lea/node.js >> /tmp/add-apt-errors 2>&1
 # python3.3
-sudo add-apt-repository -y ppa:fkrull/deadsnakes >> /tmp/add-apt-errors 2>&1
+add-apt-repository -y ppa:fkrull/deadsnakes >> /tmp/add-apt-errors 2>&1
 
 # download the package lists from the repositories
-sudo apt-get update
+apt-get update
 
 
 # --- miscellaneous ----
 
-sudo apt-get install -y curl
-sudo apt-get install -y git-core
-sudo apt-get install -y screen
-
+apt-get install -y curl git-core screen vim
 
 # --- python ---
 
 # install python3.3
-sudo apt-get install -y python3.3
+apt-get install -y python3.3
 
 # set default python version to 3.3
-sudo ln -sf /usr/bin/python3.3 /usr/bin/python
-sudo dpkg --configure python
+ln -sf /usr/bin/python3.3 /usr/bin/python
+dpkg --configure python
 
 # install setuptools
 curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -o - | sudo python
 
 # install pip using setuptools
-sudo easy_install pip
+easy_install pip
 
 # Set default pip to 3.3
-sudo ln -sf /usr/local/bin/pip3.3 /usr/bin/pip
+ln -sf /usr/local/bin/pip3.3 /usr/bin/pip
 
 
 # --- apache ---
 
 # install packages
-sudo apt-get install -y apache2
-sudo apt-get install -y libapache2-mod-rewrite
-sudo apt-get install -y libapache2-mod-php5
-sudo apt-get install -y libapache2-mod-wsgi
+apt-get install -y apache2 libapache2-mod-wsgi
 
 # remove default webroot
-sudo rm -rf /var/www
+rm -rf /var/www
 
 # symlink project as webroot
-sudo ln -fs /vagrant /var/www
+ln -fs /vagrant /var/www
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -70,9 +64,7 @@ EOF
 echo "${VHOST}" > /etc/apache2/sites-available/default
 
 # enable apache rewrite module
-sudo a2enmod rewrite
-
-
+a2enmod rewrite
 
 
 # --- mysql ---
@@ -80,7 +72,7 @@ sudo a2enmod rewrite
 # install packages
 echo mysql-server mysql-server/root_password select "vagrant" | debconf-set-selections
 echo mysql-server mysql-server/root_password_again select "vagrant" | debconf-set-selections
-sudo apt-get install -y mysql-server-5.5
+apt-get install -y mysql-server-5.5
 
 # create database
 # mysql -uroot -pvagrant -e "CREATE DATABASE test;"
@@ -88,19 +80,17 @@ sudo apt-get install -y mysql-server-5.5
 
 # --- node.js ---
 
-# install node.js dependencies
-sudo apt-get install -y g++ make
-
-# install packages
-sudo apt-get install -y nodejs
+# install node.js and dependencies
+apt-get install -y g++ make nodejs
 
 
 # --- Django ---
 
-sudo pip install Django==1.6.2
+pip install Django==1.6.2
 cd /vagrant && python manage.py syncdb
 
 
 # --- restart apache ---
 
-sudo service apache2 restart
+service apache2 restart
+echo "cd /vagrant/ && echo 'http://127.0.0.1:9000' && python manage.py runserver 0.0.0.0:8000" >> /home/vagrant/.bashrc
