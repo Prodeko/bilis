c94bf00613cce281583f4b8e9100f635efb97ca9
Matti Vesikivi <matti.vesikivi@iki.fi>
2014-05-30 16:00:24 +0300

    [delivers: #67657510] show validation errors in the UI

--- bilis/forms.py
+++ bilis/forms.py
@@ -1,4 +1,4 @@
-#coding: utf8
+﻿#coding: utf8
 from django.forms import ModelForm, TextInput, Select, ValidationError
 from django.utils.translation import ugettext_lazy as _
 from bilis.models import Player, Game
@@ -30,11 +30,21 @@ class ResultForm(ModelForm):
             'winner': Select(attrs={'id': u'winner'}),
             'loser': Select(attrs={'id': u'loser'})
         }
+        error_messages = {
+            'winner': {
+                'required': _('Valitse voittaja.'),
+            },
+            'loser': {
+                'required': _('Valitse häviäjä.'),
+            },
+        }
     def clean(self):
-
+        # winner and loser can't be the same player
         if (self.cleaned_data.get('winner') == self.cleaned_data.get('loser')):
-            raise ValidationError(
-                "Voittaja ja häviäjä ei voi olla sama pelaaja."
-            )
+            #only raise this error if the player is not None
+            if (self.cleaned_data.get('winner') is not None):
+                raise ValidationError(
+                    "Voittaja ja häviäjä ei voi olla sama pelaaja."
+                )
 
         return self.cleaned_data
